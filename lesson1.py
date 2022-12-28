996235214809377fa1651021c4316af70fbcbc50
Mr.Framon <Mr.Framon@yandex.ru>
2022-12-29 00:59:01 +0400

    Created the lesson_1 file
    1. A bot was created using Telegram BotFather
    2. The venv virtual environment has been created
    3. The gitignore file has been created
    4. An authorization token has been added to connect to the Telegram API

---
+++ lesson1.py
@@ -0,0 +1,5 @@
+import aiogram
+
+# Бот - сервер, который будет взаимодействовать с API Telegram
+
+TOKEN_API = "5602277457:AAGyHQwtGu-ZC1K1j01bVvZAqcqTz7fV4hg" # Авторизационный токен для подключения к Telegram API
